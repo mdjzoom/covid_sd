a40266ad116951377df7e90f6c86bc3dee6c160d
mdjzoom <jamazen5@gmail.com>
2020-05-13 23:55:10 +0300

    Updates

---
+++ precache-manifest.a5432ebdf2f3d2eff4952ebb23ea4ebe.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "815b41c432ac54573745b3050e917d0c",
+    "url": "/covid_sd/index.html"
+  },
+  {
+    "revision": "447d52fa12c33f1cb628",
+    "url": "/covid_sd/static/css/main.df0a9ecd.chunk.css"
+  },
+  {
+    "revision": "9ded023438ad00664994",
+    "url": "/covid_sd/static/js/2.6ee630d7.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid_sd/static/js/2.6ee630d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "447d52fa12c33f1cb628",
+    "url": "/covid_sd/static/js/main.2782af0e.chunk.js"
+  },
+  {
+    "revision": "379b456001f45d3be7c3",
+    "url": "/covid_sd/static/js/runtime-main.5206e565.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid_sd/static/media/image.d7265326.png"
+  }
+]);
